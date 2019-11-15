30de6bba58948916dfab4cde0c33390a07288f92
LavanyaJay <lavanyajay75@gmail.com>
2019-11-15 15:12:15 +0100

    Add streams dictionary

--- index.js
+++ index.js
@@ -13,19 +13,35 @@ app.get("/", (req, res, next) => {
   res.send("hello");
 });
 
+const streams = {};
+
 const stream = new Sse();
+const messages = {};
+
 app.get("/stream", (req, res, next) => {
-  const string = JSON.stringify(messages);
+  const rooms = Object.keys(messages);
+  const string = JSON.stringify(rooms);
   stream.updateInit(string);
   stream.init(req, res);
 });
 
-const messages = [];
-app.post("/message", (req, res, next) => {
+app.get("/rooms/:roomName", (req, res, next) => {
+  const { roomName } = req.params;
+  const stream = streams[roomName];
+  const data = messages[roomName];
+  const string = JSON.stringify(data);
+  stream.updateInit(string);
+  stream.init(req, res);
+});
+
+app.post("/message/:roomName", (req, res, next) => {
   const { message } = req.body;
+  const { roomName } = req.params;
+  const room = message[roomName];
+  const stream = streams[roomName];
   const string = JSON.stringify(message);
   stream.send(string);
-  messages.push(message);
+  room.push(message);
   res.send(message);
 });
 
@@ -33,6 +49,19 @@ app.get("/message", (req, res, next) => {
   res.send(messages);
 });
 
+app.post("/room", (req, res, next) => {
+  const { name } = req.body;
+  send(name);
+  rooms.push(name);
+  messages[name] = [];
+  streams[name] = new Sse();
+  res.send(name);
+});
+
+send = data => {
+  const string = JSON.stringify(data);
+  stream.send(string);
+};
 app.listen(port, () => {
   console.log("Listening on port: " + port);
 });
