953f4989fcb7daf15589ca20adb7ae7833553de7
LavanyaJay <lavanyajay75@gmail.com>
2019-11-15 10:26:11 +0100

    read all messages endpoint

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 4000;
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
 app.get("/", (req, res, next) => {
   res.send("hello");
+});
+
+const messages = [];
+app.post("/message", (req, res, next) => {
+  const { message } = req.body;
+  messages.push(message);
+  res.send(message);
+});
+
+app.get("/message", (req, res, next) => {
+  res.send(messages);
 });
 app.listen(port, () => {
   console.log("Listening on port: " + port);
